16787fc4bc12794a68975bbe5c60bd79efceb759
fcastillonubison <fcastillo@nubison.cl>
2017-11-28 14:29:33 -0300

    nubison division equipos a color

---
+++ nubison.WebProjects/Default.WebProject/__alphaLaunch.alphaLaunch/_files/login_c839ebb004834e8bb8783fa2a5aafc7f/css/GrGray/style.js
@@ -0,0 +1,715 @@
+A5.themes.add('GrGray',{
+	"panelCard": {
+		"base": {
+			"className": "",
+			"header": {
+				"className": "GrGrayPanelHeader"
+			},
+			"body": {
+				"className": "GrGrayPanelBody"
+			},
+			"footer": {
+				"className": "GrGrayPanelFooter"
+			}
+		},
+		"top": {
+			"className": "",
+			"header": {
+				"className": "GrGrayPanelTopHeader"
+			},
+			"body": {
+				"className": "GrGrayPanelTopBody"
+			},
+			"footer": {
+				"className": "GrGrayPanelTopFooter"
+			}
+		}
+	},
+	"panelNavigator": {
+		"base": {
+			"header": {
+				"className": "GrGrayPanelHeader"
+			},
+			"body": {
+				"className": "GrGrayPanelBody"
+			},
+			"footer": {
+				"className": "GrGrayPanelFooter"
+			},
+			"indicator": {
+				"className": "GrGrayPanelNavInd",
+				"panelClassName": "GrGrayPanelNavIndButton",
+				"panelSelectedClassName": "GrGrayPanelNavIndButtonSelected"
+			}
+		},
+		"top": {
+			"header": {
+				"className": "GrGrayPanelTopHeader"
+			},
+			"body": {
+				"className": "GrGrayPanelTopBody"
+			},
+			"footer": {
+				"className": "GrGrayPanelTopFooter"
+			},
+			"indicator": {
+				"className": "GrGrayPanelNavInd",
+				"panelClassName": "GrGrayPanelNavIndButton",
+				"panelSelectedClassName": "GrGrayPanelNavIndButtonSelected"
+			}
+		}
+	},
+	"panelLayout": {
+		"base": {
+			"header": {
+				"className": "GrGrayPanelHeader"
+			},
+			"body": {
+				"className": "GrGrayPanelBody"
+			},
+			"footer": {
+				"className": "GrGrayPanelFooter"
+			},
+			"dock": {
+				"flowLock": {
+					"className": "GrGrayPanelOverlay"
+				}
+			}
+		},
+		"top": {
+			"header": {
+				"className": "GrGrayPanelTopHeader"
+			},
+			"body": {
+				"className": "GrGrayPanelTopBody"
+			},
+			"footer": {
+				"className": "GrGrayPanelTopFooter"
+			},
+			"dock": {
+				"flowLock": {
+					"className": "GrGrayPanelOverlay"
+				}
+			}
+		}
+	},
+	"editButtonGroup": {
+		"base": {
+			"container": {
+				"className": "GrGrayEditBG",
+				"hoverClassName": "GrGrayEditBGHover",
+				"focusClassName": "GrGrayEditBGFocus",
+				"separatorClassName": "GrGrayEditBGSeparator",
+				"editClassName": "GrGrayEditBGEdit",
+				"button": {
+					"html": "<img src=\"css/GrGray/EditDDArrow.png\" />",
+					"className": "GrGrayEditBGButton",
+					"hoverClassName": "GrGrayEditBGButtonHover",
+					"pressedClassName": "GrGrayEditBGButtonPressed"
+				}
+			}
+		}
+	},
+	"button": {
+		"base": {
+			"className": "GrGrayButton",
+			"hoverClassName": "GrGrayButtonHover",
+			"pressedClassName": "GrGrayButtonPressed",
+			"disabledClassName": "GrGrayButtonDisabled"
+		},
+		"toolbar": {
+			"className": "GrGrayButtonT",
+			"hoverClassName": "GrGrayButtonTHover",
+			"pressedClassName": "GrGrayButtonTPressed",
+			"disabledClassName": "GrGrayButtonTDisabled"
+		}
+	},
+	"buttonDropdown": {
+		"base": {
+			"className": "GrGrayButtonDD",
+			"hoverClassName": "GrGrayButtonDDHover",
+			"pressedClassName": "GrGrayButtonDDPressed",
+			"disabledClassName": "GrGrayButtonDDDisabled",
+			"contentClassName": "GrGrayButtonDDContent",
+			"dropdown": {
+				"className": "GrGrayButtonDDDropdown",
+				"icon": "css/GrGray/ButtonDDArrow.png"
+			}
+		},
+		"toolbar": {
+			"className": "GrGrayButtonTDD",
+			"hoverClassName": "GrGrayButtonTDDHover",
+			"pressedClassName": "GrGrayButtonTDDPressed",
+			"disabledClassName": "GrGrayButtonTDDDisabled",
+			"contentClassName": "GrGrayButtonTDDContent",
+			"dropdown": {
+				"className": "GrGrayButtonTDDDropdown",
+				"icon": "css/GrGray/ButtonDDArrow.png"
+			}
+		}
+	},
+	"buttonSplit": {
+		"base": {
+			"className": "GrGrayButtonDD",
+			"hoverClassName": "GrGrayButtonDDHover",
+			"pressedClassName": "GrGrayButtonDDPressed",
+			"disabledClassName": "GrGrayButtonDDDisabled",
+			"contentClassName": "GrGrayButtonDDSplitContent",
+			"dropdown": {
+				"className": "GrGrayButtonDDSplitDropdown",
+				"hoverClassName": "GrGrayButtonDDSplitDropdownHover",
+				"icon": "css/GrGray/ButtonDDArrow.png"
+			}
+		},
+		"toolbar": {
+			"className": "GrGrayButtonTDD",
+			"hoverClassName": "GrGrayButtonTDDHover",
+			"pressedClassName": "GrGrayButtonTDDPressed",
+			"disabledClassName": "GrGrayButtonTDDDisabled",
+			"contentClassName": "GrGrayButtonTDDSplitContent",
+			"dropdown": {
+				"className": "GrGrayButtonTDDSplitDropdown",
+				"hoverClassName": "GrGrayButtonTDDSplitDropdownHover",
+				"icon": "css/GrGray/ButtonDDArrow.png"
+			}
+		}
+	},
+	"buttonList": {
+		"base": {
+			"button": {
+				"className": "GrGrayButton",
+				"firstClassName": "GrGrayButtonGroupHF",
+				"lastClassName": "GrGrayButtonGroupHL",
+				"middleClassName": "GrGrayButtonGroupHM",
+				"hoverClassName": "GrGrayButtonHover",
+				"pressedClassName": "GrGrayButtonPressed",
+				"disabledClassName": "GrGrayButtonDisabled"
+			}
+		},
+		"vertical": {
+			"button": {
+				"className": "GrGrayButton",
+				"firstClassName": "GrGrayButtonGroupVF",
+				"lastClassName": "GrGrayButtonGroupVL",
+				"middleClassName": "GrGrayButtonGroupVM",
+				"hoverClassName": "GrGrayButtonHover",
+				"pressedClassName": "GrGrayButtonPressed",
+				"disabledClassName": "GrGrayButtonDisabled"
+			}
+		}
+	},
+	"slider": {
+		"base": {
+			"className": "GrGraySliderH",
+			"innerClassName": "GrGraySliderHInner",
+			"handle": {
+				"className": "GrGraySliderHHandle",
+				"minClassName": "GrGraySliderHHandleMin",
+				"maxClassName": "GrGraySliderHHandleMax",
+				"selectedClassName": "GrGraySliderHHandleSelected",
+				"hoverClassName": "GrGraySliderHHandleHover"
+			},
+			"range": {
+				"className": "GrGraySliderHRange",
+				"selectedClassName": "GrGraySliderHRangeSelected",
+				"hoverClassName": "GrGraySliderHRangeHover"
+			}
+		}
+	},
+	"switch": {
+		"base": {
+			"className": "GrGraySwitch",
+			"onClassName": "GrGraySwitchOn",
+			"offClassName": "GrGraySwitchOff",
+			"innerClassName": "GrGraySwitchInner",
+			"rightClassName": "GrGraySwitchRight",
+			"leftClassName": "GrGraySwitchLeft",
+			"buttonClassName": "GrGraySwitchButton"
+		}
+	},
+	"spinList": {
+		"base": {
+			"className": "GrGraySpinList",
+			"item": {
+				"className": "GrGraySpinListItem"
+			}
+		},
+		"selectionBand": {
+			"className": "GrGraySpinList GrGraySpinListSelectBand",
+			"item": {
+				"className": "GrGraySpinListItem"
+			}
+		}
+	},
+	"listbox": {
+		"base": {
+			"className": "GrGrayList",
+			"focusClassName": "GrGrayListFocus",
+			"item": {
+				"className": "GrGrayListItem",
+				"hoverClassName": "GrGrayListItemHover",
+				"selectedClassName": "GrGrayListItemSelected",
+				"titleClassName": "GrGrayListTitle",
+				"separatorClassName": "GrGrayListSeparator"
+			},
+			"columnLayout": {
+				"header": {
+					"className": "GrGrayListHeader",
+					"item": {
+						"className": "GrGrayListHeaderItem",
+						"hoverClassName": "GrGrayListHeaderItemHover",
+						"order": {
+							"ascendingImage": "css/GrGray/GridSortAsc.png",
+							"descendingImage": "css/GrGray/GridSortDesc.png",
+							"style": "position: absolute; right: 6px; top: 50%; margin-top: -5px; display: none;"
+						}
+					}
+				}
+			}
+		}
+	},
+	"tree": {
+		"base": {
+			"className": "GrGrayTree",
+			"focusClassName": "GrGrayTreeFocus",
+			"node": {
+				"className": "GrGrayTreeNode",
+				"hoverClassName": "GrGrayTreeNodeHover",
+				"selectedClassName": "GrGrayTreeNodeSelected",
+				"labelClassName": "GrGrayTreeNodeLabel",
+				"leaf": {
+					"firstImage": "",
+					"image": "",
+					"lastImage": ""
+				},
+				"branch": {
+					"firstExpandedImage": "",
+					"firstCollapsedImage": "",
+					"firstExpandedHoverImage": "",
+					"firstCollapsedHoverImage": "",
+					"expandedImage": "css/GrGray/TreeBranchCollapse.png",
+					"collapsedImage": "css/GrGray/TreeBranchExpand.png",
+					"expandedHoverImage": "css/GrGray/TreeBranchCollapseHover.png",
+					"collapsedHoverImage": "css/GrGray/TreeBranchExpandHover.png",
+					"lastExpandedImage": "",
+					"lastCollapsedImage": "",
+					"lastExpandedHoverImage": "",
+					"lastCollapsedHoverImage": ""
+				}
+			},
+			"branch": {
+				"className": "GrGrayTreeBranch",
+				"lineImage": "",
+				"nullImage": "css/GrGray/TreeNull.png"
+			}
+		}
+	},
+	"datePicker": {
+		"base": {
+			"className": "GrGrayDP",
+			"calendar": {
+				"daysOfWeek": {
+					"show": true,
+					"className": "GrGrayDPDaysOfWeek"
+				},
+				"weeksOfYear": {
+					"show": false,
+					"className": "GrGrayDPWeeksOfYear",
+					"headerClassName": "GrGrayDPWeeksOfYearHeader"
+				},
+				"date": {
+					"className": "GrGrayDPDate",
+					"weekendClassName": "GrGrayDPDateWeekend",
+					"todayClassName": "GrGrayDPDateToday",
+					"item": {
+						"className": "GrGrayDPItem",
+						"hoverClassName": "GrGrayDPItemHover",
+						"selectedClassName": "GrGrayDPItemSelected",
+						"disabledClassName": "GrGrayDPItemDisabled",
+						"outOfRangeClassName": "GrGrayDPItemOutOfRange"
+					}
+				}
+			},
+			"navigator": {
+				"header": {
+					"className": "GrGrayDPHeader",
+					"prevIcon": "css/GrGray/DPPrev.png",
+					"nextIcon": "css/GrGray/DPNext.png",
+					"prevIconHover": "",
+					"nextIconHover": "",
+					"item": {
+						"className": "GrGrayDPHeaderButton",
+						"hoverClassName": "GrGrayDPHeaderButtonHover",
+						"selectedClassName": "GrGrayDPHeaderButtonSelected"
+					}
+				},
+				"panel": {
+					"className": "GrGrayDPPanel",
+					"edit": {
+						"className": "GrGrayEdit"
+					},
+					"item": {
+						"className": "GrGrayDPItem",
+						"hoverClassName": "GrGrayDPItemHover",
+						"selectedClassName": "GrGrayDPItemSelected",
+						"disabledClassName": "GrGrayDPItemDisabled"
+					}
+				}
+			}
+		}
+	},
+	"timePicker": {
+		"base": {
+			"className": "GrGrayTP",
+			"edit": {
+				"className": "GrGrayEdit",
+				"buttonClassName": "GrGrayTPEditButton",
+				"buttonImage": "css/GrGray/TPEditDDArrow.png"
+			},
+			"meridianClassName": "GrGrayTPMeridianButton"
+		}
+	},
+	"menu": {
+		"base": {
+			"className": "GrGrayMenu",
+			"innerClassName": "GrGrayMenuInner",
+			"closeOnLeave": true,
+			"closeWait": 500,
+			"cascadeOffsetX": 0,
+			"cascadeOffsetY": 0,
+			"iconColumn": {
+				"className": "GrGrayMenuIconColumn",
+				"preventIndentClassName": "GrGrayMenuNoIndent",
+				"width": "22px"
+			},
+			"item": {
+				"className": "GrGrayMenuItem",
+				"labelClassName": "GrGrayMenuItemLabel",
+				"hoverClassName": "GrGrayMenuItemHover",
+				"selectedClassName": "GrGrayMenuItemSelected",
+				"disabledClassName": "GrGrayMenuItemDisabled",
+				"disabledHoverClassName": "GrGrayMenuItemDisabledHover",
+				"cascadeClassName": "GrGrayMenuItemCascade",
+				"separatorClassName": "GrGrayMenuSeparator",
+				"titleClassName": "GrGrayMenuTitle",
+				"radioImage": "css/GrGray/MenuRadio.png",
+				"checkImage": "css/GrGray/MenuCheck.png"
+			},
+			"pointer": {
+				"size": 15,
+				"upClassName": "GrGrayMenuPointerUp",
+				"leftClassName": "GrGrayMenuPointerLeft",
+				"downClassName": "GrGrayMenuPointerDown",
+				"rightClassName": "GrGrayMenuPointerRight"
+			}
+		}
+	},
+	"menubar": {
+		"base": {
+			"item": {
+				"className": "GrGrayMenubarHItem",
+				"hoverClassName": "GrGrayMenubarHItemHover",
+				"pressedClassName": "GrGrayMenubarHItemPressed",
+				"disabledClassName": "GrGrayMenubarHItemDisabled",
+				"cascadeClassName": "GrGrayMenubarHItemCascade",
+				"separatorClassName": "GrGrayMenubarHSeparator"
+			}
+		},
+		"vertical": {
+			"item": {
+				"className": "GrGrayMenubarVItem",
+				"hoverClassName": "GrGrayMenubarVItemHover",
+				"pressedClassName": "GrGrayMenubarVItemPressed",
+				"disabledClassName": "GrGrayMenubarVItemDisabled",
+				"cascadeClassName": "GrGrayMenubarVItemCascade",
+				"separatorClassName": "GrGrayMenubarVSeparator"
+			}
+		}
+	},
+	"window": {
+		"base": {
+			"className": "GrGrayWin",
+			"outerWrapClassName": "GrGrayWinInner",
+			"innerWrapClassName": "GrGrayWinBodyContainer",
+			"adjustmentClassName": "GrGrayWinAdjust",
+			"activeClassName": "GrGrayWinActive",
+			"lockUIClassName": "GrGrayUILock",
+			"title": {
+				"className": "GrGrayWinTitle",
+				"tools": {
+					"inset": "4px"
+				}
+			},
+			"header": {
+				"className": "GrGrayWinHeader"
+			},
+			"tbar": {
+				"className": "GrGrayWinTBAR"
+			},
+			"body": {
+				"className": "GrGrayWinBody"
+			},
+			"bbar": {
+				"className": "GrGrayWinBBAR"
+			},
+			"buttons": {
+				"className": "GrGrayWinButtons"
+			},
+			"footer": {
+				"className": "GrGrayWinFooter"
+			},
+			"resizer": {
+				"className": "GrGrayWinResizer",
+				"thumbImage": "css/GrGray/WinResizeThumb.png"
+			},
+			"pointer": {
+				"size": 15,
+				"upClassName": "GrGrayWinPointerUp",
+				"leftClassName": "GrGrayWinPointerLeft",
+				"downClassName": "GrGrayWinPointerDown",
+				"rightClassName": "GrGrayWinPointerRight"
+			},
+			"_buttonDefaults": {
+				"className": "GrGrayButton",
+				"hoverClassName": "GrGrayButtonHover",
+				"pressedClassName": "GrGrayButtonPressed",
+				"disabledClassName": "GrGrayButtonDisabled"
+			},
+			"_defaultTools": {
+				"close": {
+					"image": "css/GrGray/WinClose.png",
+					"imageHover": "css/GrGray/WinCloseHover.png",
+					"imagePressed": "css/GrGray/WinClosePressed.png",
+					"action": "close"
+				}
+			}
+		},
+		"dropdown": {
+			"className": "GrGrayDDWin",
+			"outerWrapClassName": "GrGrayDDWinInner",
+			"innerWrapClassName": "GrGrayDDWinBodyContainer",
+			"adjustmentClassName": "GrGrayDDWinAdjust",
+			"activeClassName": "GrGrayDDWinActive",
+			"lockUIClassName": "GrGrayUILock",
+			"title": {
+				"className": "GrGrayDDWinTitle",
+				"tools": {
+					"inset": "4px"
+				}
+			},
+			"header": {
+				"className": "GrGrayDDWinHeader"
+			},
+			"tbar": {
+				"className": "GrGrayDDWinTBAR"
+			},
+			"body": {
+				"className": "GrGrayDDWinBody"
+			},
+			"bbar": {
+				"className": "GrGrayDDWinBBAR"
+			},
+			"buttons": {
+				"className": "GrGrayDDWinButtons"
+			},
+			"footer": {
+				"className": "GrGrayDDWinFooter"
+			},
+			"resizer": {
+				"className": "GrGrayDDWinResizer",
+				"thumbImage": "css/GrGray/DDWinResizeThumb.png"
+			},
+			"pointer": {
+				"size": 15,
+				"upClassName": "GrGrayDDWinPointerUp",
+				"leftClassName": "GrGrayDDWinPointerLeft",
+				"downClassName": "GrGrayDDWinPointerDown",
+				"rightClassName": "GrGrayDDWinPointerRight"
+			},
+			"_buttonDefaults": {
+				"className": "GrGrayButton",
+				"hoverClassName": "GrGrayButtonHover",
+				"pressedClassName": "GrGrayButtonPressed",
+				"disabledClassName": "GrGrayButtonDisabled"
+			},
+			"_defaultTools": {
+				"close": {
+					"image": "css/GrGray/DDWinClose.png",
+					"imageHover": "css/GrGray/DDWinCloseHover.png",
+					"imagePressed": "css/GrGray/DDWinClosePressed.png",
+					"action": "close"
+				}
+			}
+		},
+		"panel": {
+			"className": "GrGrayPanelCard",
+			"activeClassName": "",
+			"adjustmentClassName": "",
+			"outerWrapClassName": "",
+			"innerWrapClassName": "",
+			"lockUIClassName": "GrGrayUILock",
+			"title": {
+				"className": "GrGrayPanelTopHeader"
+			},
+			"header": {
+				"className": "GrGrayPanelHeader"
+			},
+			"tbar": {
+				"className": "GrGrayPanelHeader"
+			},
+			"body": {
+				"className": "GrGrayPanelBody"
+			},
+			"buttons": {
+				"className": "GrGrayPanelFooter"
+			},
+			"bbar": {
+				"className": "GrGrayPanelFooter"
+			},
+			"footer": {
+				"className": "GrGrayPanelFooter"
+			},
+			"resizer": {
+				"className": "GrGrayDDWinResizer",
+				"thumbImage": "css/GrGray/WinResizeThumb.png"
+			},
+			"_defaultTools": {
+				"close": {
+					"action": "close",
+					"name": "close",
+					"image": "css/GrGray/WinClose.png"
+				}
+			}
+		}
+	},
+	"accordion": {
+		"base": {
+			"titleClassName": "GrGrayAccordionButton",
+			"titleSelectedClassName": "GrGrayAccordionButtonSelected",
+			"titleDisabledClassName": "GrGrayAccordionButtonDisabled",
+			"paneClassName": "GrGrayAccordionPane"
+		},
+		"icon": {
+			"titleClassName": "GrGrayAccordionIconButton",
+			"titleSelectedClassName": "GrGrayAccordionIconButtonSelected",
+			"titleDisabledClassName": "GrGrayAccordionIconButtonDisabled",
+			"paneClassName": "GrGrayAccordionPane"
+		}
+	},
+	"tab": {
+		"base": {
+			"tabClassName": "GrGrayTabTButton",
+			"tabSelectedClassName": "GrGrayTabTButtonSelected",
+			"tabDisabledClassName": "GrGrayTabTButtonDisabled",
+			"paneClassName": "GrGrayTabTPane"
+		},
+		"bottom": {
+			"tabClassName": "GrGrayTabBButton",
+			"tabSelectedClassName": "GrGrayTabBButtonSelected",
+			"tabDisabledClassName": "GrGrayTabBButtonDisabled",
+			"paneClassName": "GrGrayTabBPane"
+		},
+		"left": {
+			"tabClassName": "GrGrayTabLButton",
+			"tabSelectedClassName": "GrGrayTabLButtonSelected",
+			"tabDisabledClassName": "GrGrayTabLButtonDisabled",
+			"paneClassName": "GrGrayTabLPane"
+		},
+		"right": {
+			"tabClassName": "GrGrayTabRButton",
+			"tabSelectedClassName": "GrGrayTabRButtonSelected",
+			"tabDisabledClassName": "GrGrayTabRButtonDisabled",
+			"paneClassName": "GrGrayTabRPane"
+		}
+	},
+	"tabBand": {
+		"base": {
+			"location": "top",
+			"className": "GrGrayTabbandT",
+			"separator": {
+				"className": "GrGrayTabbandTSeparator",
+				"show": true
+			},
+			"tab": {
+				"className": "GrGrayTabbandTButton",
+				"selectedClassName": "GrGrayTabbandTButtonSelected",
+				"disabledClassName": "GrGrayTabbandTButtonDisabled",
+				"closeIcon": "css/GrGray/TabbandClose.png",
+				"closeIconHover": "css/GrGray/TabbandCloseHover.png"
+			},
+			"scrollButtons": {
+				"prevClassName": "GrGrayTabbandTLScroll",
+				"prevDisabledClassName": "GrGrayTabbandTLScrollDisabled",
+				"prevImage": "css/GrGray/TabbandPrev.png",
+				"prevDisabledImage": "css/GrGray/TabbandPrevDisabled.png",
+				"nextClassName": "GrGrayTabbandTRScroll",
+				"nextDisabledClassName": "GrGrayTabbandTRScrollDisabled",
+				"nextImage": "css/GrGray/TabbandNext.png",
+				"nextDisabledImage": "css/GrGray/TabbandNextDisabled.png"
+			}
+		},
+		"bottom": {
+			"location": "bottom",
+			"className": "GrGrayTabbandB",
+			"separator": {
+				"className": "GrGrayTabbandBSeparator",
+				"show": true
+			},
+			"tab": {
+				"className": "GrGrayTabbandBButton",
+				"selectedClassName": "GrGrayTabbandBButtonSelected",
+				"disabledClassName": "GrGrayTabbandBButtonDisabled",
+				"closeIcon": "css/GrGray/TabbandClose.png",
+				"closeIconHover": "css/GrGray/TabbandCloseHover.png"
+			},
+			"scrollButtons": {
+				"prevClassName": "GrGrayTabbandBLScroll",
+				"prevDisabledClassName": "GrGrayTabbandBLScrollDisabled",
+				"prevImage": "css/GrGray/TabbandPrev.png",
+				"prevDisabledImage": "css/GrGray/TabbandPrevDisabled.png",
+				"nextClassName": "GrGrayTabbandBRScroll",
+				"nextDisabledClassName": "GrGrayTabbandBRScrollDisabled",
+				"nextImage": "css/GrGray/TabbandNext.png",
+				"nextDisabledImage": "css/GrGray/TabbandNextDisabled.png"
+			}
+		}
+	},
+	scroller: {
+		base: {
+			className: 'GrGrayScroller',
+			focusClassName: 'GrGrayScrollerFocus',
+			dialClassName: 'GrGrayScrollerDialExpanded',
+			dial: {className: 'GrGrayScrollerDial'},
+			slider: {
+				className: 'GrGrayScrollerSlider',
+				handle: {
+					className: 'GrGrayScrollerSliderHandle'
+				},
+				shadow: {
+					className: 'GrGrayScrollerSliderShadow'
+				},
+				message: {
+					className: 'GrGrayScrollerSliderMsg',
+					bottomClassName: 'GrGrayScrollerSliderMsgB',
+					topClassName: 'GrGrayScrollerSliderMsgT',
+					rightClassName: 'GrGrayScrollerSliderMsgR',
+					leftClassName: 'GrGrayScrollerSliderMsgL'
+				}
+			}
+		}
+	},
+	"scroll": {
+		"base": {
+			"indicator": {
+				"h": {
+					"className": "GrGrayScrollIndH",
+					"barClassName": "GrGrayScrollIndHBar"
+				},
+				"v": {
+					"className": "GrGrayScrollIndV",
+					"barClassName": "GrGrayScrollIndVBar"
+				}
+			}
+		}
+	}
+});
